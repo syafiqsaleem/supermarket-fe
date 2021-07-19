fec055f13822d71f3ed6d059517b27cfbe1de426
doomvell13 <wknight13@gmail.com>
2021-07-19 14:46:41 +0800

    able to show categories on shop left sidebar

--- supermarket-fe/src/Routes.jsx
+++ supermarket-fe/src/Routes.jsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Signup from "./components/user/Signup";
-import Signin from "./components/user/Signin";
-import Home from "./components/core/Home";
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Signup from './components/user/Signup'
+import Signin from './components/user/Signin'
+import Home from './components/core/Home'
+import Shop from './components/core/Shop'
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/shop" exact component={Shop} />
         <Route path="/signin" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
       </Switch>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default Routes;
+export default Routes
--- supermarket-fe/src/components/core/Shop.jsx
+++ supermarket-fe/src/components/core/Shop.jsx
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import Layout from './Layout'
-import Card from './Card'
+import { getCategories } from './ApiController'
 
 const Shop = () => {
+  const [categories, setCategories] = useState([])
+  const [error, setError] = useState(false)
+
+  const init = () => {
+    getCategories().then((data) => {
+      if (error) {
+        setError(error)
+      } else {
+        setCategories(data)
+      }
+    })
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
+
   return (
     <Layout
       title="See all Products"
@@ -10,9 +27,10 @@ const Shop = () => {
       className="container-fluid"
     >
       <div className="row">
-        <div className="col-4"> Left sidebar</div>
+        <div className="col-4"> {JSON.stringify(categories)}</div>
         <div className="col-8">Right Content</div>
       </div>
     </Layout>
   )
 }
+export default Shop
