4fe25ffe6d52465411406c7ba3000b45a8922c92
syafiqsaleem <19syafiq89@gmail.com>
2021-07-23 00:00:03 +0800

    Fetch all orders for admin

--- supermarket-fe/src/Routes.jsx
+++ supermarket-fe/src/Routes.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Signup from './components/user/Signup'
-import Signin from './components/user/Signin'
-import Home from './components/core/Home'
-import Dashboard from './components/user/UserDashboard'
-import AdminDashboard from './components/user/AdminDashboard'
-import Shop from './components/core/Shop'
-import Product from './components/core/Product'
-import Cart from './components/core/Cart'
-import PrivateRoute from './auth/PrivateRoute'
-import AdminRoute from './auth/AdminRoute'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from "./components/user/Signup";
+import Signin from "./components/user/Signin";
+import Home from "./components/core/Home";
+import Dashboard from "./components/user/UserDashboard";
+import AdminDashboard from "./components/user/AdminDashboard";
+import Shop from "./components/core/Shop";
+import Product from "./components/core/Product";
+import Cart from "./components/core/Cart";
+import PrivateRoute from "./auth/PrivateRoute";
+import AdminRoute from "./auth/AdminRoute";
+import Orders from "./components/admin/ordersAdmin";
 
 const Routes = () => {
   return (
@@ -23,9 +24,10 @@ const Routes = () => {
         <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
         <Route path="/cart" exact component={Cart} />
         <Route path="/product/:productId" exact component={Product} />
+        <AdminRoute path="/admin/orders" exact component={Orders} />
       </Switch>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Routes
+export default Routes;
