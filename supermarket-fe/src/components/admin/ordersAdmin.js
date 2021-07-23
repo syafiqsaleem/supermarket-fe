284ae9f9fba3612265d99f689baa3265fcb4d315
syafiqsaleem <19syafiq89@gmail.com>
2021-07-23 11:03:30 +0800

    Show product details of each order

--- supermarket-fe/src/components/admin/ordersAdmin.js
+++ supermarket-fe/src/components/admin/ordersAdmin.js
@@ -33,6 +33,16 @@ const Orders = () => {
       return <h1 className="text-danger">No orders</h1>;
     }
   };
+
+  const showInput = (key, value) => (
+    <div className="input-group mb-2 mr-sm-2">
+      <div className="input-group-prepend">
+        <div className="input-group-text">{key}</div>
+      </div>
+      <input type="text" value={value} className="form-control" readOnly />
+    </div>
+  );
+
   return (
     <Layout
       title="Orders"
@@ -71,6 +81,22 @@ const Orders = () => {
                 <h3 className="mt-4 mb-4 font-italic">
                   Total products in the order: {o.products.length}
                 </h3>
+
+                {o.products.map((p, pIndex) => (
+                  <div
+                    className="mb-4"
+                    key={pIndex}
+                    style={{
+                      padding: "20px",
+                      border: "1px solid indigo",
+                    }}
+                  >
+                    {showInput("Product name", p.name)}
+                    {showInput("Product price", p.price)}
+                    {showInput("Product total", p.count)}
+                    {showInput("Product Id", p._id)}
+                  </div>
+                ))}
               </div>
             );
           })}
