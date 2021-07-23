767c98b8ef846cd1fc9e52487c0a9c0d63111fd5
syafiqsaleem <19syafiq89@gmail.com>
2021-07-23 21:52:20 +0800

    user profile update methods

---
+++ supermarket-fe/src/components/user/profile.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../core/Layout";
+import { isAuthenticated } from "../auth";
+import { Link } from "react-router-dom";
+
+// We need 2 methods, 1) Read the user information from the backend, 2)Update the user information
+const Profile = () => {
+  return <h2>profile</h2>;
+};
+
+export default Profile;
