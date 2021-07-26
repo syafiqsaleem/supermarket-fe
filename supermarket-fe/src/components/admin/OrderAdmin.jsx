9f4937db33e329e41cbd74e1564d570683d6c395
doomvell13 <wknight13@gmail.com>
2021-07-26 18:12:51 +0800

    minor changes to admin orders page

--- supermarket-fe/src/components/admin/OrderAdmin.jsx
+++ supermarket-fe/src/components/admin/OrderAdmin.jsx
@@ -1,54 +1,54 @@
-import React, { useState, useEffect } from "react";
-import Layout from "../core/Layout";
-import { isAuthenticated } from "../auth";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import Layout from '../core/Layout'
+import { isAuthenticated } from '../auth'
+import { Link } from 'react-router-dom'
 import {
   listOrders,
   getStatusValues,
   updateOrderStatus,
-} from "./AdminApiController";
-import moment from "moment";
+} from './AdminApiController'
+import moment from 'moment'
 
 const Orders = () => {
-  const [orders, setOrders] = useState([]);
-  const [statusValues, setStatusValues] = useState([]);
+  const [orders, setOrders] = useState([])
+  const [statusValues, setStatusValues] = useState([])
 
-  const { user, token } = isAuthenticated();
+  const { user, token } = isAuthenticated()
 
   const loadOrders = () => {
     listOrders(user._id, token).then((data) => {
       if (data.error) {
-        console.log(data.error);
+        console.log(data.error)
       } else {
-        setOrders(data);
+        setOrders(data)
       }
-    });
-  };
+    })
+  }
 
   const loadStatusValues = () => {
     getStatusValues(user._id, token).then((data) => {
       if (data.error) {
-        console.log(data.error);
+        console.log(data.error)
       } else {
-        setStatusValues(data);
+        setStatusValues(data)
       }
-    });
-  };
+    })
+  }
 
   useEffect(() => {
-    loadOrders();
-    loadStatusValues();
-  }, []);
+    loadOrders()
+    loadStatusValues()
+  }, [])
 
   const showOrdersLength = () => {
     if (orders.length > 0) {
       return (
         <h1 className="text-danger display-2">Total orders: {orders.length}</h1>
-      );
+      )
     } else {
-      return <h1 className="text-danger">No orders</h1>;
+      return <h1 className="text-danger">No orders</h1>
     }
-  };
+  }
   // Due to the number of divs to be written, a function, 'showInput' is used to keep the code clean
   const showInput = (key, value) => (
     <div className="input-group mb-2 mr-sm-2">
@@ -57,17 +57,17 @@ const Orders = () => {
       </div>
       <input type="text" value={value} className="form-control" readOnly />
     </div>
-  );
+  )
 
   const handleStatusChange = (e, orderId) => {
     updateOrderStatus(user._id, token, orderId, e.target.value).then((data) => {
       if (data.error) {
-        console.log("Status update failed");
+        console.log('Status update failed')
       } else {
-        loadOrders();
+        loadOrders()
       }
-    });
-  };
+    })
+  }
 
   const showStatus = (o) => (
     <div className="form-group">
@@ -84,12 +84,12 @@ const Orders = () => {
         ))}
       </select>
     </div>
-  );
+  )
 
   return (
     <Layout
       title="Orders"
-      description={`G'day ${user.name}, you can manage all the orders here`}
+      description={`Hey ${user.name}, you can manage all the orders here`}
     >
       <div className="row">
         <div className="col-md-8 offset-md-2">
@@ -101,7 +101,7 @@ const Orders = () => {
               <div
                 className="mt-5"
                 key={oIndex}
-                style={{ borderBottom: "5px solid indigo" }}
+                style={{ borderBottom: '5px solid indigo' }}
               >
                 <h2 className="mb-5">
                   <span className="bg-primary">Order ID: {o._id}</span>
@@ -132,23 +132,23 @@ const Orders = () => {
                     className="mb-4"
                     key={pIndex}
                     style={{
-                      padding: "20px",
-                      border: "1px solid indigo",
+                      padding: '20px',
+                      border: '1px solid indigo',
                     }}
                   >
-                    {showInput("Product name", p.name)}
-                    {showInput("Product price", p.price)}
-                    {showInput("Product total", p.count)}
-                    {showInput("Product Id", p._id)}
+                    {showInput('Product name', p.name)}
+                    {showInput('Product price', p.price)}
+                    {showInput('Product total', p.count)}
+                    {showInput('Product Id', p._id)}
                   </div>
                 ))}
               </div>
-            );
+            )
           })}
         </div>
       </div>
     </Layout>
-  );
-};
+  )
+}
 
-export default Orders;
+export default Orders
