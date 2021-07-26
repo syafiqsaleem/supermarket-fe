aa94acd7e3045b7f0a78245d07b2df729d916605
doomvell13 <wknight13@gmail.com>
2021-07-26 18:28:01 +0800

    change shop page products to list to 8 items

--- supermarket-fe/src/components/core/Shop.jsx
+++ supermarket-fe/src/components/core/Shop.jsx
@@ -13,7 +13,7 @@ const Shop = () => {
   })
   const [categories, setCategories] = useState([])
   const [error, setError] = useState(false)
-  const [limit, setLimit] = useState(6)
+  const [limit, setLimit] = useState(8)
   const [skip, setSkip] = useState(0)
   const [size, setSize] = useState(0)
   const [filteredResults, setFilteredResults] = useState([])
@@ -138,8 +138,8 @@ const Shop = () => {
                 {filteredResults.map((product, i) => (
                   <Card key={i} product={product} />
                 ))}
+                {loadMoreButton()}
               </div>
-              {loadMoreButton()}
             </div>
           </div>
         </div>
